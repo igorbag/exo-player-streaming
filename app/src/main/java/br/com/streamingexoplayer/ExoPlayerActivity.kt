a6d84b447f284b0517e0d6624324483ea9f69778
Igor Rotondo Bagliotti <igor.bagliotti@gmail.com>
2019-10-18 23:02:38 -0300

    Adicionando componente de player

---
+++ app/src/main/java/br/com/streamingexoplayer/ExoPlayerActivity.kt
@@ -0,0 +1,12 @@
+package br.com.streamingexoplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExoPlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exo_player)
+    }
+}
